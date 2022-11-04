bc926293b44a18ef2cef39a94affadeead9a52cf
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.fr>
2022-11-04 12:15:56 +0100

    securistaion admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -9,6 +9,14 @@ class AdminController extends AbstractController
      */
     public function index(): string
     {
+        if (!isset($_SESSION['user_id'])) {
+            echo 'Unauthorized access';
+            header('HTTP/1.1 401 Unauthorized');
+            return $this->twig->render('Error/error.html.twig', [
+                'error' => '401',
+            ]);
+        }
+
         return $this->twig->render('Admin/index.html.twig');
     }
 }
