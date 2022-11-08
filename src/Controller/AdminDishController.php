0eb208d27201e50de9059ebd1bfef1d69a971aff
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.fr>
2022-11-08 15:01:21 +0100

    add dish

--- src/Controller/AdminDishController.php
+++ src/Controller/AdminDishController.php
@@ -6,13 +6,68 @@ use App\Model\DishManager;
 
 class AdminDishController extends AbstractController
 {
+    public const INPUT_MAX_LENGTH = 255;
+
     public function index(): string
     {
+        $this->isAuthenticated();
+
         $dishManager = new DishManager();
         $dishes = $dishManager->selectAll('title');
 
         return $this->twig->render('Admin/Dish/index.html.twig', [
             'dishes' => $dishes,
         ]);
+    }
+
+    public function add(): string
+    {
+        $errors = $dish = [];
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $dish = array_map('trim', $_POST);
+            $errors = $this->validate($dish);
+
+            if (empty($errors)) {
+                $dishManager = new DishManager();
+                $dishManager->insert($dish);
+
+                header('Location: /admin/menu');
+                return '';
+            }
+        }
+
+        return $this->twig->render('Admin/Dish/add.html.twig', [
+            'errors' => $errors,
+            'dish' => $dish,
+        ]);
+    }
+
+
+    private function validate(array $dish): array
+    {
+        $errors = [];
+
+        if (empty($dish['title'])) {
+            $errors[] = 'Le nom du plat est obligatoire';
+        }
+
+        if (empty($dish['price'])) {
+            $errors[] = 'Le prix du plat est obligatoire';
+        }
+
+        if (!is_numeric($dish['price']) || $dish['price'] < 0) {
+            $errors[] = 'Le prix doit être un nombre positif';
+        }
+
+        if (strlen($dish['title']) > self::INPUT_MAX_LENGTH) {
+            $errors[] = 'Le titre doit faire moins de ' . self::INPUT_MAX_LENGTH . ' caractères';
+        }
+
+        if (strlen($dish['description']) > self::INPUT_MAX_LENGTH) {
+            $errors[] = 'La description doit faire moins de ' . self::INPUT_MAX_LENGTH . ' caractères';
+        }
+
+        return $errors;
     }
 }
--- src/Model/DishManager.php
+++ src/Model/DishManager.php
@@ -7,4 +7,17 @@ use PDO;
 class DishManager extends AbstractManager
 {
     public const TABLE = 'dish';
+
+    public function insert(array $dish): void
+    {
+        $query = "INSERT INTO " . self::TABLE .
+            " (`title`, `description`, `price`) 
+            VALUES (:title, :description, :price)";
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue('title', $dish['title'], PDO::PARAM_STR);
+        $statement->bindValue('description', $dish['description'], PDO::PARAM_STR);
+        $statement->bindValue('price', $dish['price']);
+
+        $statement->execute();
+    }
 }
